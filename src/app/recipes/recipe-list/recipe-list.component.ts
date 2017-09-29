54346231e32ac56dc37ba0fd492c712b7e15b6e4
Thomas McClellan <thomasmcclellan87@gmail.com>
2017-09-29 14:47:55 -0400

    Added dummy data to recipe list and header

--- src/app/recipes/recipe-list/recipe-list.component.ts
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Recipe } from '../recipe.model';
 
 @Component({
   selector: 'app-recipe-list',
@@ -6,6 +7,9 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./recipe-list.component.css']
 })
 export class RecipeListComponent implements OnInit {
+  recipes: Recipe[] = [
+    new Recipe('A Test Recipe', 'this is a test', 'https://www.google.com/url?sa=i&rct=j&q=&esrc=s&source=images&cd=&ved=0ahUKEwjq0vOchsvWAhXllFQKHQaVBGAQjRwIBw&url=https%3A%2F%2Fwww.bbcgoodfood.com%2Frecipes%2Fcollection%2Feasy&psig=AFQjCNFf-2bTjtE13FlTBOkieG-sPfr5_A&ust=1506796938811775')
+  ];
 
   constructor() { }
 
